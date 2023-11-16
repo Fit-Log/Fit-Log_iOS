7905b4afc5610dac5d63fdf42b97379345171e18
Choyoungjun <cyj070513@gmail.com>
2023-11-16 23:26:40 +0900

    📑 :: MenuView 생성

---
+++ FitLog/FitLog/Sources/Component/MenuView.swift
@@ -0,0 +1,57 @@
+import UIKit
+import SnapKit
+import Then
+
+class MenuView: UIView {
+    
+    let label = UILabel().then {
+        $0.font = .IBMPlexSansFont(font: .semiBold, ofSize: 20)
+        $0.textColor = .gray700
+    }
+    let editButton = UIButton(type: .system).then {
+        $0.setImage(.editIcon, for: .normal)
+        $0.tintColor = .gray600
+    }
+    let kcalLabel = UILabel().then {
+        $0.text = "Kcal"
+        $0.font = .IBMPlexSansFont(font: .semiBold, ofSize: 18)
+        $0.textColor = .main700
+    }
+    
+    
+    init(time: String) {
+        super.init(frame: .zero)
+        label.text = time
+        setting()
+        layout()
+    }
+    
+    func setting() {
+        self.backgroundColor = .white
+        self.layer.border(color: .main400, width: 1)
+        self.layer.cornerRadius = 12
+        self.clipsToBounds = true
+    }
+    func layout() {
+        [
+            label,
+            editButton,
+            kcalLabel,
+        ].forEach({ self.addSubview($0) })
+        label.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.left.equalToSuperview().inset(20)
+        }
+        editButton.snp.makeConstraints {
+            $0.top.right.equalToSuperview().inset(12)
+        }
+        kcalLabel.snp.makeConstraints {
+            $0.right.bottom.equalToSuperview().inset(12)
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
