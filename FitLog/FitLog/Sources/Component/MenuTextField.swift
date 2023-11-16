d1909065ec49c4141526bf96577009c85aa07095
Choyoungjun <cyj070513@gmail.com>
2023-11-16 23:26:17 +0900

    📑 :: MenuTextField 추가

---
+++ FitLog/FitLog/Sources/Component/MenuTextField.swift
@@ -0,0 +1,36 @@
+import UIKit
+
+class MenuTextField: UITextField {
+    
+    var placeholderText: String = ""
+    
+    init(
+        placeholder: String
+    ) {
+        super.init(frame: .zero)
+        self.backgroundColor = .gray100
+        self.textColor = .gray800
+        self.font = .IBMPlexSansFont(font: .medium, ofSize: 14)
+        self.placeholderText = placeholder
+        let spacerView = UIView(frame: CGRect(x: 0, y: 0, width: 16, height: 0))
+        self.leftView = spacerView
+        self.leftViewMode = .always
+        self.layer.cornerRadius = 8
+        setting()
+    }
+    
+    func setting() {
+            self.attributedPlaceholder = NSAttributedString(
+                string: placeholderText,
+                attributes: [
+                    NSAttributedString.Key.foregroundColor: UIColor.gray,
+                    NSAttributedString.Key.font: UIFont.IBMPlexSansFont(font: .medium, ofSize: 14)
+                ]
+            )
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
