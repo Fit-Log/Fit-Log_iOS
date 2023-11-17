ca5fbb3cd7f999b134f8e547e82ad2563ff34116
Choyoungjun <cyj070513@gmail.com>
2023-11-17 16:59:10 +0900

    ⚡️ :: 메뉴 입력 기능 및 tableView 생성

--- FitLog/FitLog/Sources/Scene/Menu/DetailMenuViewController.swift
+++ FitLog/FitLog/Sources/Scene/Menu/DetailMenuViewController.swift
@@ -2,9 +2,12 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-class DetailMenuViewController: BaseVC {
+class DetailMenuViewController: BaseVC {//MARK: 입력한 메뉴를 서버에 보내줘야하는지 로컬에서 처리해야하는지 당연히 서버일거같긴한데
     
     private let disposeBag = DisposeBag()
+    
+    var menuArray: [String] = []
+    var kcalArray: [String] = []
     
     private let mainLogoImage = UIImageView(image: .logo)
     private let mealTimeLabel = UILabel().then {
@@ -21,10 +24,19 @@ class DetailMenuViewController: BaseVC {
         $0.setImage(.addIcon, for: .normal)
         $0.tintColor = .gray700
     }
+    private let menuTextField = MenuTextField(placeholder: "메뉴 입력")
+    private let kcalTextField = MenuTextField(placeholder: "칼로리 입력")
+    private let menuTableView = UITableView().then {
+        $0.backgroundColor = .white
+        $0.register(MenuTableViewCell.self, forCellReuseIdentifier: "MenuTableViewCell")
+        $0.rowHeight = 48
+        $0.separatorStyle = .none
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
     }
     override func configureUI() {
         super.configureUI()
@@ -33,7 +45,10 @@ class DetailMenuViewController: BaseVC {
             mainLogoImage,
             mealTimeLabel,
             addMenuLabel,
-            addMenuButton
+            addMenuButton,
+            menuTextField,
+            kcalTextField,
+            menuTableView
         ].forEach({ view.addSubview($0) })
         
     }
@@ -60,13 +75,66 @@ class DetailMenuViewController: BaseVC {
             $0.right.equalToSuperview().inset(20)
             $0.width.height.equalTo(30)
         }
+        menuTextField.snp.makeConstraints {
+            $0.top.equalTo(addMenuLabel.snp.bottom).offset(25)//강해민한테 물어보고 레이아웃 조정하기
+            $0.left.right.equalToSuperview().inset(20)
+            $0.height.equalTo(40)
+        }
+        kcalTextField.snp.makeConstraints {
+            $0.top.equalTo(menuTextField.snp.bottom).offset(8)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.height.equalTo(40)
+        }
+        menuTableView.snp.makeConstraints {
+            $0.top.equalTo(kcalTextField.snp.bottom).offset(40)
+            $0.left.right.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(20)
+        }
         
     }
     override func subscribe() {
         super.subscribe()
         
-        
+        addMenuButton.rx.tap
+            .bind(onNext: {
+                self.menuArray.append(self.menuTextField.text!)
+                self.kcalArray.append(self.kcalTextField.text!)
+                self.menuTableView.reloadData()
+            }).disposed(by: disposeBag)
         
     }
     
+}
+
+
+extension DetailMenuViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menuArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = menuTableView.dequeueReusableCell(withIdentifier: "MenuTableViewCell", for: indexPath) as! MenuTableViewCell
+        cell.selectionStyle = .none
+        cell.menuLabel.text = self.menuArray[indexPath.row]
+        cell.kcalLabel.text = self.kcalArray[indexPath.row]
+        cell.deleteButton.rx.tap
+            .bind(onNext: {
+                self.menuArray.remove(at: indexPath.row)
+                self.kcalArray.remove(at: indexPath.row)
+                self.menuTableView.reloadData()
+            }).disposed(by: disposeBag)
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+//            menuTableView.deleteRows(at: [indexPath], with: .automatic) 
+        self.menuArray.remove(at: indexPath.row)
+        self.kcalArray.remove(at: indexPath.row)
+        menuTableView.reloadData()
+    }
+//    
 }
